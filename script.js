9c28f4b89c567748a780380908924d08ccdd8095
Your Name <you@example.com>
2021-02-08 16:54:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let table = document.getElementById("table");
+let elements = document.querySelectorAll("td");
+
+let clicks = 0;
+let cellNumber = "1";
+let cellColor = "red";
+
+table.onclick = function (event) {
+  let td = event.target.closest("td");
+  if (!td) return;
+  if (!table.contains(td)) return;
+  cellNumber = td.textContent;
+};
+
+button.onclick = function () {
+  cellColor = document.getElementById("myColor").value;
+  for (let elem of elements) {
+    if (elem.textContent === cellNumber) {
+      elem.style.backgroundColor = "white";
+    } else {
+      elem.style.backgroundColor = cellColor;
+    }
+  }
+};
+
+
